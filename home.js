1c856187e1d0e5d736823d24ef447b8ecd7e1ae6
Trevaughn Daley <trevaughn13@gmail.com>
2015-10-16 21:05:29 -0400

    home with initial banners
    .

---
+++ home.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#tweet").click(function(){
+        $("#essay1").show();
+        
+    });
+});
+
+
+function addtweet() {
+    var x = document.getElementById("tweet");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
